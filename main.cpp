2f8856e66cd4162846d49b0ed2c560d08a6a7fa7
edcu11 <edgarcu2010@hotmail.com>
2015-08-26 12:07:36 -0600

    Masiso

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int opc=0;
+    while(opc!=5){
+        cout<<"1.Sumar"<<endl;
+        cout<<"2.Restar"<<endl;
+        cout<<"3.Multiplicar"<<endl;
+        cout<<"4.Dividir"<<endl;
+        cout<<"5.Salir"<<endl;
+
+
+
+
+
+    }
+}
